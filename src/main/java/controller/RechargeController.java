19e871821a9dd832e87ae6ae2513cb4dd73f5c2e
sulbha-06 <sulbha06@gmil.com>
2018-04-28 23:39:21 +0530

    pom.xml- spring jars

--- src/main/java/controller/RechargeController.java
+++ src/main/java/controller/RechargeController.java
@@ -1,10 +1,14 @@
 package controller;
-@controller
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RechargeController {
-@RequestMapping("/hello")
-public ModelAndView helloWorld {
-	String message = "hello welcome to recharge service";
-	return new ModelAndView("hellopage","mesage",message);
-	
-}
+
+	@RequestMapping("/hello")
+	public String helloWorld() {
+		String message = "hello welcome to recharge service";
+		return message;
+	}
 }
